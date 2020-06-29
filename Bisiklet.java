779383c638719c0689ba263dcb074a96a0ac8409
ahmetyanik <ahmetyank4242@gmail.com>
2020-06-29 20:53:44 +0200

    ilk calismami sunuyorum

---
+++ Bisiklet.java
@@ -0,0 +1,15 @@
+package bisiklet;
+
+public class Bisiklet {
+
+	public static void main(String[] args) {
+
+		System.out.println("bugün ilk gün ve calismaya basladik");
+		System.out.println("günün sonu geldi ne yaptik patrona gönderelim");
+		System.out.println("simdi bir patronun fikrini alalim");
+		
+		
+		
+	}
+
+}
